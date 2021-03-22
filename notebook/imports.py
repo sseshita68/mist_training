814dbe24e4a746f70d024db7cf3e51145d4db0b9
“RabbaniM” <ayon.rab@gmail.com>
2021-03-22 09:13:30 -0400

    notebook

---
+++ notebook/imports.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn import preprocessing 
